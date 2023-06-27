c6559c10ac69afdee2bb63157fc325ae62c2b9b0
Hanminho <hihi6024@gmail.com>
2023-06-27 15:42:44 +0900

    disqus 연동 및 설정

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+DISQUS_SHORTNAME = 'mydjangoproject'
+DISQUS_MY_DOMAIN='http:/127.0.0.1:8000'
+
 # TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]

8169332fa748a19cae15fe770d19c77e5767ad1e
Hanminho <hihi6024@gmail.com>
2023-06-28 15:14:36 +0900

    photo app additions

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from photo.models import Album
+
 
 # Create your views here.
+class AlbumLV(ListView):
+    model = Album
